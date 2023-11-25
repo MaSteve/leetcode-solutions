8e8de0c57f27d1c7e824fe5ba70f6d9b9b21d24f
Marco Antonio Garrido Rojo <marco@raincoatmoon.com>
2023-11-25 10:04:38 +0000

    1685. Sum of Absolute Differences in a Sorted Array
    https://leetcode.com/problems/sum-of-absolute-differences-in-a-sorted-array/

---
+++ src/bin/1685.rs
@@ -0,0 +1,14 @@
+pub fn get_sum_absolute_differences(nums: Vec<i32>) -> Vec<i32> {
+    let sum: i32 = nums.iter().sum();
+    nums.iter()
+        .enumerate()
+        .scan((sum, 0), |(s1, s2), (i, v)| {
+            let res = *s1 - (nums.len() as i32 - i as i32 - 1) * v + ((i as i32 - 1) * v - *s2);
+            *s1 -= v;
+            *s2 += v;
+            Some(res)
+        })
+        .collect()
+}
+
+fn main() {}
