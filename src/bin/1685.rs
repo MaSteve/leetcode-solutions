075f8b9ce6e5ff7bd126754ed334e33f0aa71e74
Marco Antonio Garrido Rojo <marco@raincoatmoon.com>
2023-11-25 10:11:02 +0000

    Simplify 1685

--- src/bin/1685.rs
+++ src/bin/1685.rs
@@ -3,7 +3,7 @@ pub fn get_sum_absolute_differences(nums: Vec<i32>) -> Vec<i32> {
     nums.iter()
         .enumerate()
         .scan((sum, 0), |(s1, s2), (i, v)| {
-            let res = *s1 - (nums.len() as i32 - i as i32 - 1) * v + ((i as i32 - 1) * v - *s2);
+            let res = *s1 - (nums.len() as i32 - 2 * i as i32) * v - *s2;
             *s1 -= v;
             *s2 += v;
             Some(res)
